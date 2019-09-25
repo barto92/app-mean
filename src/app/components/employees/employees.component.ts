b327fa39a6b12ef1738c3a12c88f4f8eee6e7a25
Armando C <barto.rock@gmail.com>
2019-09-24 23:02:34 -0500

    Update and delete employee

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { EmployeeService } from "../../services/employee.service";
+import { NgForm } from "@angular/forms";
+import { Employee } from "src/app/models/employee";
+
+declare var M: any;
 
 @Component({
   selector: "app-employees",
@@ -10,5 +14,47 @@ import { EmployeeService } from "../../services/employee.service";
 export class EmployeesComponent implements OnInit {
   constructor(private employeeService: EmployeeService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  resetForm = (form?: NgForm) => {
+    if (form) {
+      form.reset();
+      this.employeeService.selectedEmployee = new Employee();
+    }
+  };
+
+  addEmployee = (form: NgForm) => {
+    if (form.value._id) {
+      this.employeeService.putEmployee(form.value).subscribe(res => {
+        this.resetForm(form);
+        M.toast({ html: "Updated Succesfully" });
+        this.getEmployees();
+      });
+    } else {
+      this.employeeService.postEmployee(form.value).subscribe(res => {
+        this.resetForm(form);
+        M.toast({ html: "Saved Succesfully" });
+        this.getEmployees();
+      });
+    }
+  };
+
+  getEmployees = () => {
+    this.employeeService.getEmployees().subscribe(res => {
+      this.employeeService.employees = res as Employee[];
+    });
+  };
+
+  editEmployee = (employee: Employee) => {
+    this.employeeService.selectedEmployee = employee;
+  };
+
+  deleteEmployee = (_id: string) => {
+    this.employeeService.deleteEmployee(_id).subscribe(res => {
+      M.toast({ html: "Deleted Succesfully" });
+      this.getEmployees();
+    });
+  };
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Employee } from "../models/employee";
+import { API } from "../cfg/api";
 
 @Injectable({
   providedIn: "root"
@@ -8,25 +9,24 @@ import { Employee } from "../models/employee";
 export class EmployeeService {
   selectedEmployee: Employee;
   employees: Employee[];
-  readonly URLAPI = "http://localhost:3000/api/";
 
   constructor(private http: HttpClient) {
     this.selectedEmployee = new Employee();
   }
 
   getEmployees = () => {
-    return this.http.get(this.URLAPI + "employees");
+    return this.http.get(API.url + "employees/");
   };
 
   postEmployee = (employee: Employee) => {
-    return this.http.post(this.URLAPI + "employees", employee);
+    return this.http.post(API.url + "employees/", employee);
   };
 
   putEmployee = (employee: Employee) => {
-    return this.http.put(this.URLAPI + "employee/" + employee._id, employee);
+    return this.http.put(API.url + "employees/" + employee._id, employee);
   };
 
   deleteEmployee = (_id: string) => {
-    return this.http.delete(this.URLAPI + "employee/" + _id);
+    return this.http.delete(API.url + "employees/" + _id);
   };
 }
